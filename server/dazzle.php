05919f83384b0d544a6315b718a2a63174029e4c
ClickyButton <3boodxbox@gmail.com>
2017-03-11 12:59:04 +0300

    Add files via upload

--- server/dazzle.php
+++ server/dazzle.php
@@ -9,20 +9,21 @@
 //   return false;
 //    }
 
+$d=mktime(11, 14, 54, 8, 12, 2014);
+$todaydate=date("Y-m-d h:i:sa", $d);
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $location = $_POST['location'];
-$carmake = $_POST['carmake'];
-$caryear = $_POST['caryear'];
-$carmodel = $_POST['carmodel'];
 $carsize = $_POST['carsize'];
+$phone = $_POST['phone'];
+$plate = $_POST['plate'];
 $services = $_POST['services'];
 
 
 // Create the email and send the message
-$to = 'man.rs85@gmail.com'; // Add your email address inbetween the '' replacing 
-$email_subject = "Dr.Dazzle Booking";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nlocation:\n$location\n\ncar make:\n$carmake\n\ncar year:\n$caryear\n\ncar model:\n$carmodel\n\ncar size:\n$car size\n\nservices:\n$services";
+$to = 'b.5.d@hotmail.com'; // Add your email address inbetween the '' replacing
+$email_subject = "Dr.Dazzle Booking" . $todaydate ;
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nlocation:\n$location\n\ncar size:\n$carsize \n\ncar plate:\n$plate\n\nservices:\n$services";
 $headers = "From: noreply@DrDazzel.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 mail($to,$email_subject,$email_body,$headers);
 echo "sent!";
